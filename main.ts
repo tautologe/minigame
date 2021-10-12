4ce1573eebf2e915fbb675ee98c07b9809758a4b
tautologe <tautologe@users.noreply.github.com>
2021-10-12 23:30:47 +0200

    enable fast mode

--- main.ts
+++ main.ts
@@ -1,22 +1,27 @@
 input.onButtonPressed(Button.A, function () {
     ship.move(-1)
 })
+input.onButtonPressed(Button.AB, function () {
+    interval = 100
+})
 input.onButtonPressed(Button.B, function () {
     ship.move(1)
 })
+let interval = 0
 let ship: game.LedSprite = null
 let sprite = game.createSprite(2, 0)
 sprite.turn(Direction.Right, 90)
 ship = game.createSprite(2, 4)
+interval = 500
 basic.forever(function () {
-    basic.pause(500)
+    basic.pause(interval)
     sprite.move(1)
     if (ship.isTouching(sprite)) {
         game.gameOver()
     }
     if (sprite.get(LedSpriteProperty.Y) == 4) {
         game.setScore(game.score() + 1)
-        basic.pause(500)
+        basic.pause(interval)
         sprite.set(LedSpriteProperty.X, randint(0, 4))
         sprite.set(LedSpriteProperty.Y, 0)
     }
